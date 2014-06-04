0e3a4265abb70c11dbb8342c404dec35ca3a0b64
whusted <whusted@lion.lmu.edu>
2014-06-03 22:37:09 -0700

    Initial Commit

---
+++ Peek/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Peek
+//
+//  Created by Willy Husted on 6/3/14.
+//  Copyright (c) 2014 Willy Husted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
